509c218b49e124db9b1e34e28de0b26d02b9deab
Kushagar Sharma <kushagars08@gmail.com>
2025-11-05 19:17:37 +0530

    langsmith tracking setup

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import openai
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+## Langsmith tracking setup
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_PROJECT"] = "Q&A Chatbot with OpenAI"
+
