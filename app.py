4fe4164c0a84bad9f4e750ca75614cdf2f8ac0ae
Kushagar Sharma <kushagars08@gmail.com>
2025-11-05 19:24:04 +0530

    generate response function

--- app.py
+++ app.py
@@ -14,3 +14,18 @@ os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
 os.environ["LANGCHAIN_PROJECT"] = "Q&A Chatbot with OpenAI"
 
+## Prompt template
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful assistant please reponse to the user queries."),
+        ("user", "Question: {question}"),
+    ]
+)
+
+def generate_response(question, api_key, llm, temperature, max_tokens):
+    openai.api_key = api_key
+    llm = ChatOpenAI(model=llm, temperature=temperature, max_tokens=max_tokens)
+    output_parser = StrOutputParser()
+    chain = prompt | llm | output_parser
+    answer = chain.invoke({"question": question})
+    return answer
